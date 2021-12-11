27b6e0e11926edadf8b975eb3d06f6d13f4207f1
adrian4448 <adrian.morais4448@gmail.com>
2021-12-10 22:09:03 -0300

    Ajustando problema do localhost

--- src/common/plugins/axios.js
+++ src/common/plugins/axios.js
@@ -10,7 +10,7 @@ Vue.use({
         Vue.prototype.$http.interceptors.response.use(res => res.data, 
             error => {
                 if(error.response.status == 401) {
-                   global.location.href = `${window.location.origin}/login`;
+                   global.location.href = `${global.location.origin}/login`;
                 }
             });
     }
